c4ae8add920f59a58b853f8542b080ac4a0d46aa
Swapnil Kathale <127191602+swapnilk06@users.noreply.github.com>
2024-11-05 23:33:34 +0530

    Update SQL_Exercise.sql

--- SQL_Exercise.sql
+++ SQL_Exercise.sql
@@ -2,3 +2,14 @@ SQL Select -
 => 
 select * FROM Customers;
 
+---------------------------
+
+Write a statement that will select the City column from the Customers table. -
+=>
+select City from Customers;
+
+--------------------------
+
+Select all the different values from the Country column in the Customers table.
+  
+
